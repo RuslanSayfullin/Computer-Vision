c8f8ac757f23edafcdb5b95dc3e442e2b67da90a
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-05-18 09:55:27 +0500

    Метод map::max_size()

---
+++ max_size_method.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+	// initiaalize container
+	map<int, int> mp1, mp2;
+	mp1.insert({ 1, 2 });
+
+	// max size of non-empty map
+	cout << "The max size of mp1 is " << mp1.max_size();
+
+	// max size of Empty-map
+	// cout << "\nThe max size of mp2 is " << mp2.max_size();
+	return 0;
+}
+// The max size of mp1 is .......
