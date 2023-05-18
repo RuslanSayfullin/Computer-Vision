46c79d53f9cf30f011157c3f4f108b652ed54c8a
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-05-18 17:38:41 +0500

    Всякое

---
+++ conditional_variable_example.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <thread>
+#include <condition_variable>
+
+std::condition_variable cv;
+std::mutex mtx;
+bool ready = false;
+
+void worker()
+{
+	std::cout << "Worker: Waiting for a signal..." << std::endl;
+
+	std::unique_lock<std::mutex> lock(mtx);
+	cv.wait(lock, []{ return ready; });
+
+	std::cout << "Worker: Got the signal!" << std::endl;
+}
+
+int main()
+{
+	std::thread workerThread(worker);
+
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+
+	{
+		std::lock_guard<std::mutex> lock(mtx);
+		ready = true;
+		cv.notify_one();
+	}
+
+	workerThread.join();
+
+	return 0;
+}
